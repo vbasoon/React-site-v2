b335452b8fded5dc1d33f65ec5b9668c59076f1f
Басун Володимир <vbasoon@gmail.com>
2020-09-23 12:06:01 +0300

    checkButtonSize correct

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -15,7 +15,7 @@ export const Button = ({
 }) => {
    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
-   const   = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
    return (
       <Link to='/sign-up' className='btn-mobile'>
          <button
