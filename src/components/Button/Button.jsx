1758f24a8a34f4e33310a353aa4692622333d575
Басун Володимир <vbasoon@gmail.com>
2020-09-23 12:04:26 +0300

    corect checkButtonSize

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,22 +2,32 @@ import React from 'react'
 import './Button.css'
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline']
+const STYLES = ['btn--primary', 'btn--outline'];
 
-const Button = ({
+const SIZES = ['btn--medium', 'btn--large'];
+
+export const Button = ({
    children,
    type,
    onClick,
    buttonStyle,
    buttonSize
 }) => {
-   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-   return (
-      <div>
+   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
-      </div>
+   const   = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+   return (
+      <Link to='/sign-up' className='btn-mobile'>
+         <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+         >
+            {children}
+         </button>
+
+      </Link>
    )
 }
 
-export default Button
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '../Button/Button';
+import './Navbar.css'
 
-const Navbar = () => {
+function Navbar() {
    const [click, setClick] = useState(false);
+   const [button, setButton] = useState(true);
 
    const handleClick = () => setClick(!click);
    const closeMobileMenu = () => setClick(false);
+
+   const showButton = () => {
+      if (window.innerWidth <= 960) {
+         setButton(false)
+      } else {
+         setButton(true);
+      }
+   }
+
+   useEffect(() => {
+      showButton();
+   }, []);
+
+   window.addEventListener('resize', showButton)
+
    return (
       <>
          <nav className="navbar">
             <div className="navbar-container">
                <Link to="/" className="navbar-logo">
-                  TRVL-Tour <i className="fab fa-typo3"></i>
+                  TRVL <i className="fab fa-typo3"></i>
                </Link>
                <div className="menu-icon" onClick={handleClick}>
                   <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -32,12 +50,13 @@ const Navbar = () => {
                         Products
                      </Link>
                   </li>
-                  <li className="nav-item">
+                  <li>
                      <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu} >
                         Sign Up
                      </Link>
                   </li>
                </ul>
+               {button && <Button buttonStyle='btn--outline' >SIGN UP</Button>}
             </div>
          </nav>
       </>
