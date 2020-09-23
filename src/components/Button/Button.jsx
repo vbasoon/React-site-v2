46c8e93d57c99ef6cf3c8887c2da3085000b227d
Басун Володимир <vbasoon@gmail.com>
2020-09-23 08:21:06 +0300

    styles btn

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Button.css'
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline']
+
+const Button = ({
+   children,
+   type,
+   onClick,
+   buttonStyle,
+   buttonSize
+}) => {
+   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+   return (
+      <div>
+
+      </div>
+   )
+}
+
+export default Button
+
